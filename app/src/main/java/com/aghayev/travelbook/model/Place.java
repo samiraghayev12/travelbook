2d3cbb4f6a7040426d085522cca3401318ec24de
Samir <samiragazad6@gmail.com>
2021-06-17 16:18:59 +0400

    commit changes

---
+++ app/src/main/java/com/aghayev/travelbook/model/Place.java
@@ -0,0 +1,16 @@
+package com.aghayev.travelbook.model;
+
+import java.io.Serializable;
+
+public class Place  implements Serializable {
+    public String name;
+    public Double latitude;
+    public Double longitude;
+
+    public Place(String name, Double latitude, Double longitude){
+        this.name = name;
+        this.latitude = latitude;
+        this.longitude = longitude;
+
+    }
+}
